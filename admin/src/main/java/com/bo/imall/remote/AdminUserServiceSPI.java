763dfff5bce2643dfc481f19a6dcf57172e41388
PerasonBo <18856306321@163.com>
2019-05-20 10:26:46 +0800

    add adminUserService SPI

---
+++ admin/src/main/java/com/bo/imall/remote/AdminUserServiceSPI.java
@@ -0,0 +1,8 @@
+package com.bo.imall.remote;
+
+import com.bo.imall.model.admin.AdminUser;
+
+public interface AdminUserServiceSPI {
+
+    void register(AdminUser adminUser);
+}
---
+++ admin/src/main/java/com/bo/imall/remote/AdminUserServiceSPIAdapter.java
@@ -0,0 +1,25 @@
+package com.bo.imall.remote;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.bo.imall.model.admin.AdminUser;
+import com.bo.imall.service.AdminUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class AdminUserServiceSPIAdapter implements AdminUserServiceSPI {
+
+    @Reference(version = "1.0.0")
+    private AdminUserService adminUserService;
+
+
+    @Override
+    public void register(AdminUser adminUser) {
+        try {
+            adminUserService.register(adminUser);
+        } catch (Exception e) {
+            log.error("com.bo.imall.service.AdminUserService.register  调用异常 param adminUser:{}", adminUser, e);
+        }
+    }
+}
